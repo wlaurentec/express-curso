bdf043f1c29c3d579899b9aed54ef9aac0b9662e
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-17 18:38:59 -0500

    handle request body

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/user", (req, res) => {
+  console.log("Body:", req.body);
+  res.send("Nuevo usuario creado!");
 });
 
-app.get("/data/miarchivo", (req, res) => {
-  res.sendFile(__dirname + "/luis-banchero.txt");
-});
 
-app.get("/data/lbr", (req, res) => {
-  res.sendFile("/lbr.json", { root: __dirname });
-});
-
-app.get("/isAlive", (req, res) => {
-  res.send("I'm alive!");
-});
-
-app.get("/user", (req, res) => {
-  res.json({
-    name: "Luis",
-    age: 25,
-    country: "Peru",
-    city: "Lima",
-    married: false,
-  });
-});
 
 app.listen(3000, () => {
   console.log(`Listening on http://localhost:${3000}`);
